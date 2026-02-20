0e2d1d035c950b122f888612d3adec3b3b6e0d41
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-02-20 11:23:04 -0800

    blog models and admin  were created

--- conf/blog/admin.py
+++ conf/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Post, Comment, Category
 # Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status', 'created_date','published_date']
+    search_fields = ['title', 'category']
+    list_filter = ['status', 'published_date']
+admin.site.register(Post, AdminPost)
+
+
+class AdminComment(admin.ModelAdmin):
+    list_display = ['post', 'is_approved', 'created_date']
+admin.site.register(Comment, AdminComment)
+
+admin.site.register(Category)
--- conf/blog/models.py
+++ conf/blog/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Post(models.Model):
+    # author =
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to="Posts/" ,null=True, blank=True)
+    # like =
+    status = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_snippet(self):
+        return self.content[0:5]
+
+    
+    class Meta:
+        ordering = ['-published_date']
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+
+class Comment(models.Model):
+    # user = 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
+    name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    title = models.CharField(max_length=250)
+    message = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created_date']
