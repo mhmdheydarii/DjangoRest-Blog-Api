a19bb2d84688cf81b2b88f1d5af5965e2cc54696
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-02-22 05:59:07 -0800

    accounts models (AbstractBaseUser), admin (UserAdmin)

--- conf/accounts/admin.py
+++ conf/accounts/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 # Register your models here.
+
+class CoustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_staff',  'is_superuser', "is_verified"]
+    list_filter = ['is_superuser',]
+    ordering = ['-created_date']
+    search_fields = ['email',]
+
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        (
+        "Permissions",
+            {"fields": ("is_staff", "is_superuser", "is_active", "is_verified")},
+        ),
+        ("Groupe Permissions", {"fields": ("groups", "user_permissions")}),
+        ("Important Date", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+admin.site.register(User, CoustomUserAdmin)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'created_date']
+    search_fields = ['first_name',]
+
+admin.site.register(Profile, ProfileAdmin)
--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Coustom User
+AUTH_USER_MODEL = 'accounts.User'
