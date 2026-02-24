669cee462a0c489e8b0bdd8e10b812d8de8c2e59
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-02-24 00:49:34 -0800

    Add default login,refresh and verify classes to accounts app

--- conf/accounts/api/v1/urls.py
+++ conf/accounts/api/v1/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, 
-    TokenRefreshView)
+    TokenRefreshView,
+    TokenVerifyView)
 from . import views
 
 app_name = 'api'
 
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name='register'),
-    path('token/', TokenObtainPairView.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh'),
+    path('verify/', TokenVerifyView.as_view(), name='verify')
 ]
--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 
-# Authentication Class 
+# Authentication  
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
